5751953352fac07dbe7cbfe5021fa23370365d05
duynn9433 <92941533+duynn9433@users.noreply.github.com>
2023-03-17 01:00:16 +0700

    I18n

--- src/main/java/com/duynn/todoweb/hello/SayHelloController.java
+++ src/main/java/com/duynn/todoweb/hello/SayHelloController.java
@@ -1,13 +1,23 @@
 package com.duynn.todoweb.hello;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Locale;
+
+@RestController
 public class SayHelloController {
-    @RequestMapping("/hello")
-    public String sayHello(){
-        return "sayHello";
+    private MessageSource messageSource;
 
+    public SayHelloController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @RequestMapping("/hello-world")
+    public String helloworld(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default message", locale);
     }
 }
